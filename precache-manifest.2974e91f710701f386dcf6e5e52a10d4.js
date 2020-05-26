b4e685b073c367793cdac356771beba7fda1840c
Ronny Hsieh <1020ronny@gmail.com>
2020-05-26 19:14:51 +0800

    Updates

---
+++ precache-manifest.2974e91f710701f386dcf6e5e52a10d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32f4c25d7e3021fa2c310c674dadd9dc",
+    "url": "/react-form-maker/index.html"
+  },
+  {
+    "revision": "da01375537edf23b1e08",
+    "url": "/react-form-maker/static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "cacc36415456e3398ccd",
+    "url": "/react-form-maker/static/js/2.54d4520b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-form-maker/static/js/2.54d4520b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da01375537edf23b1e08",
+    "url": "/react-form-maker/static/js/main.6f4d6130.chunk.js"
+  },
+  {
+    "revision": "82589b05682f169c6ef8",
+    "url": "/react-form-maker/static/js/runtime-main.66769bf6.js"
+  }
+]);
