42701c951971889092c6a4a209b9313ea13e3f83
Ronny Hsieh <1020ronny@gmail.com>
2020-05-26 17:39:54 +0800

    init

---
+++ src/FormItem.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const FormItem = ({ text }) => {
+  return <div>Example Component: {text}</div>
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import styles from './styles.module.css'
+import FormItem from './FormItem'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ReactFormMaker = ({ text }) => {
+  return <div>Example Component: {text}</div>
 }
