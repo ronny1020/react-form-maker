fa21120c9cf8590050b35ec8f3384ec24a32518b
Ronny Hsieh <1020ronny@gmail.com>
2020-05-28 22:27:28 +0800

    Updates

--- precache-manifest.85e73184beab478ee7484b125acbaa2d.js
+++ precache-manifest.85e73184beab478ee7484b125acbaa2d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c52a0585ad74d9d592a49c3d2ed5030d",
+    "revision": "dbc758ff907a6b6677efc004c8744111",
     "url": "/react-form-maker/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-form-maker/static/css/2.0e1a4c73.chunk.css"
   },
   {
-    "revision": "9691448ca3cc2380b4de",
+    "revision": "5cfa48b5682bda0a3e4f",
     "url": "/react-form-maker/static/css/main.c246279e.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-form-maker/static/js/2.93019bb0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9691448ca3cc2380b4de",
-    "url": "/react-form-maker/static/js/main.68696d1b.chunk.js"
+    "revision": "5cfa48b5682bda0a3e4f",
+    "url": "/react-form-maker/static/js/main.8eec92ea.chunk.js"
   },
   {
     "revision": "82589b05682f169c6ef8",
